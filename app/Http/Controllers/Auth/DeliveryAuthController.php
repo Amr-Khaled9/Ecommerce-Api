a92b3b63c62c617c7f52865f074e34794c393056
Amr-khaled9 <amrkhaled500@gmail.com>
2025-09-06 13:37:19 +0300

    add multi-auth with Spatie Permission integration

---
+++ app/Http/Controllers/Auth/DeliveryAuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class DeliveryAuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        // validation
+
+        //hash password
+        $password = Hash::make($request->password);
+        // insert in DB
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $password,
+            'type' => 'delivery'
+        ]);
+        //generate token
+        $token = $user->createToken('auth_token')->plainTextToken;  //$request->device_name
+        // return response
+        return response()->json(['message' => 'User register Successfully', 'user' => $user, 'token' => $token], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User Login Successfully',
+            'user' => $user->only(['id','name','email']),
+            'token' => $token,
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        //revoke token
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'User logged out Successfully' ], 200);
+    }
+    public function profile(Request $request)
+    {
+        return response()->json(['message: profile',$request->user()],200);
+    }
+
+    public function getAccessToken(Request $request)
+    {
+        return response()->json([
+            'message' => 'token',
+            'token'   => $request->user()->currentAccessToken()
+        ], 200);
+    }
+
+}
